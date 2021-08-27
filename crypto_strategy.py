2edf24cdfa907bb4f2a2001c9caea58fd1838fb0
Beilx <72884350+Beilx@users.noreply.github.com>
2021-08-27 17:33:34 +0100

    Update crypto_strategy.py

--- crypto_strategy.py
+++ crypto_strategy.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 
 st.title("Defi Strategy Calculator: Hodling vs Stake vs Farm")
+st.markdown("![Alt Text](<iframe src="https://giphy.com/embed/3ov9jNziFTMfzSumAw" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/usnationalarchives-vintage-computer-3ov9jNziFTMfzSumAw">via GIPHY</a></p>)")
 
 st.write("""
 Calculate Returns on cryptoassets for staking, farming or simply hodling. The default positions show a scenario as follows:\n
